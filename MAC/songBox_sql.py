62409dab12dddef77d339b5627ca84e060331c36
10soos17 <shinsh1017@gmail.com>
2021-01-31 05:59:45 +0900

    edit sql function

--- MAC/songBox_sql.py
+++ MAC/songBox_sql.py
@@ -20,12 +20,89 @@ singerListDir = os.path.join(mp3Dir, "2_dir_singerlist")#userlist 폴더
 ttsDir = os.path.join(mp3Dir, "3_tts")#tts 폴더
 ignoreFile = ['ffmpeg','.DS_Store','list_data','0_file_data','1_dir_userlist','2_dir_singerlist','3_tts']#dir 안에 존재해야만하는 파일 but mp3 아닌 파일 목록
 
-#===============youtube down받고 sql db songbox->song table 에 저장================
+songDir = "/Users/soos/Desktop/git/4.web_song/hobby_songbox_web/song/" #songBox_web song 폴더
+
+#==============sql db song, singer 값 가져오기=====================================
+def get_data():
+    sql_singerlist = []
+    sql_songlist = []
+    new=""
+
+    conn = sql.connect(host='localhost', user='', password='', charset='utf8', db='songbox')
+    cursor = conn.cursor()
+
+    sql_singer = "SELECT name FROM singer;"
+    cursor.execute(sql_singer)
+    res = cursor.fetchall() #fetchone()은 열 하나만
+
+    for i in res:
+        for j in i:
+            if j != ",":
+                new+=j
+
+        sql_singerlist.append(new)
+        new=""
+
+    sql_song = "SELECT song FROM song;"
+    cursor.execute(sql_song)
+    res = cursor.fetchall()
+
+    for i in res:
+        for j in i:
+            if j != ",":
+                new+=j
+
+        sql_songlist.append(new)
+        new=""
+
+    conn.close()
+
+    return sql_singerlist, sql_songlist
+
+#==============한번에 데어터 넣을 때만 사용하기=========================================
+#==============sql db와 현재 songDir안에 곡 비교해서 데이터 넣기->======================
+#저장되어있지 않는 것만 song, singer table 에 저장=====================================
+
+def sync_sqlWebsong():
+
+    sql_singerlist, sql_songlist = get_data()
+    print(f"{sql_singerlist}\n{sql_songlist}")
+
+    conn = sql.connect(host='localhost', user='', password='', charset='utf8', db='songbox')
+    cursor = conn.cursor()
+
+    for i in os.listdir(f"{songDir}"):
+        temp = i.split("_")
+        song = temp[0]
+
+        temp = temp[1].split(".")
+        singer = temp[0]
+        filetype = temp[1]
+
+        if (song in sql_songlist and singer not in sql_singerlist) or song not in sql_songlist:
+            sql_query = "insert into song(song,filetype,singer,created) values(%s,%s,%s,NOW())"
+            cursor.execute(sql_query,(song,filetype,singer))
+            conn.commit()
+            sql_songlist.append(song)
+            print(f"song:{song}\nfile:{filetype}")
+            time.sleep(1)
+
+        if singer not in sql_singerlist:
+            sql_query = "insert into singer(name,created) values(%s,NOW())"
+            cursor.execute(sql_query,(singer))
+            sql_singerlist.append(singer)
+            print(f"singer:{singer}")
+            time.sleep(1)
+
+    conn.close()
+
+#===============youtube down받고 sql db songbox->song table 에 저장 ->webSongdir로 파일 복사===
 def down_song(song, singer, filetype):
+
     singerlist = sbs.show_singer();
 
     #sql연결
-    conn = sql.connect(host='', user='', password='', charset='utf8', db='')
+    conn = sql.connect(host='localhost', user='', password='', charset='utf8', db='songbox')
 
     # server에 연결에 성공하면, connection객체로부터 cursor()메소드를 통해 cursor 객체를 가져올 수 있음.
     cursor = conn.cursor()
@@ -40,7 +117,7 @@ def down_song(song, singer, filetype):
     print("test_:", song, singer, filetype)
 
     if singer.lower() not in singerlist:
-        print("test_singerlist:", singerlist)
+        print("singerlist:", singerlist)
         print("singer_", singer.lower(),singer)
 
         sql_query = "insert into singer(name,created) values(%s,NOW())"
@@ -49,73 +126,11 @@ def down_song(song, singer, filetype):
 
         conn.commit()
 
-    conn.close()
-"""
-def save_singer(name,country,year,created):
-
-    singerlist = sbs.show_singer();
-    print("singerlist:", singerlist)
-
-    if name.lower() not in singerlist:
-        #sql연결
-        conn = sql.connect(host='', user='', password='', charset='utf8', db='')
-
-        # server에 연결에 성공하면, connection객체로부터 cursor()메소드를 통해 cursor 객체를 가져올 수 있음.
-        cursor = conn.cursor()
-
-        sql_query = "insert into singer(name,country,year,created) values(%s,%s,%s,NOW())"
-        #cursor 객체의 execute 메서드를 이용해서 sql  문장을 실행할 수 있음.
-        cursor.execute(sql_query,(name,country,year))
-
-        # 실행한 sql 결과를 서버에 확정 반영하기
-        conn.commit()
-
-        print("test_:", name,country,year)
-
-        conn.close()
-
-def save_song(song,filetype,genre,feat,lyrics,singer,year,created):
-
-    #sql연결
-    conn = sql.connect(host='', user='', password='', charset='utf8', db='')
-
-    # server에 연결에 성공하면, connection객체로부터 cursor()메소드를 통해 cursor 객체를 가져올 수 있음.
-    cursor = conn.cursor()
-
-    sql_query = "insert into song(song,filetype,singer,created) values(%s,%s,%s,%s,%s,%s,%s,NOW())"
-    #cursor 객체의 execute 메서드를 이용해서 sql  문장을 실행할 수 있음.
-    cursor.execute(sql_query,(song,filetype,singer))
-
-    # 실행한 sql 결과를 서버에 확정 반영하기
-    conn.commit()
-
-    print("test_:", song, singer, filetype)
+    move_websong(song, singer, filetype)
 
     conn.close()
 
-def test_singer(name):
-
-    singerlist = sbs.show_singer();
-    print("singerlist:", singerlist)
-
-    if name.lower() not in singerlist:
-        #sql연결
-        conn = sql.connect(host='', user='', password='', charset='utf8', db='')
-
-        # server에 연결에 성공하면, connection객체로부터 cursor()메소드를 통해 cursor 객체를 가져올 수 있음.
-        cursor = conn.cursor()
-
-        sql_query = "insert into singer(name,created) values(%s,NOW())"
-        #cursor 객체의 execute 메서드를 이용해서 sql  문장을 실행할 수 있음.
-        cursor.execute(sql_query,(name))
-
-        # 실행한 sql 결과를 서버에 확정 반영하기
-        conn.commit()
-
-        print("test_:", name)
-
-        conn.close()
-"""
-#down_song("Stairway To Heaven","The OJays", "wav")
-#save_song(song,filetype,genre,feat,lyrics,singer,year,created)
-#save_singer(name,country,year,created)
+#===============webSongdir로 파일 복사=============================================
+def move_websong(song, singer, filetype):
+    src = f"{song}_{singer}.{filetype}"
+    shutil.copy(f"{mp3Dir}/{src}",f"{songDir}/{src}")
